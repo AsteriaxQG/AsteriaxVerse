9c1671ab30218907c6e9337ad750b8c098281405
AsteriaxQG <bachimontd@gmail.com>
2026-09-01 18:47:49 +0200

    Load automatic news script on Cloudflare build

--- build-cloudflare.sh
+++ build-cloudflare.sh
@@ -8,4 +8,9 @@ cp data/asteriax_sc.db website/data/asteriax_sc.db
 cp assets/asteriax_mark.png website/assets/asteriax_mark.png
 cp assets/asteriax_logo.png website/assets/asteriax_logo.png
 
+# Charge le flux d'actualités RSI automatique sans alourdir index.html.
+if ! grep -q 'news.js' website/index.html; then
+  sed -i 's#</body>#<script src="news.js"></script></body>#' website/index.html
+fi
+
 echo "AsteriaxVerse Web prêt dans website/"
